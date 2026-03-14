58468dd5c83781e3ec230a25b4803c0d3138e639
Ayush <ayush-devhub@gmail.com>
2026-03-14 17:55:59 +0530

    feat(config): include cart and accuont project routes in project urls

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("products.urls")),
+    path("api/", include("cart.urls")),
+    path("api/auth", include("accounts.urls")),
 ]
