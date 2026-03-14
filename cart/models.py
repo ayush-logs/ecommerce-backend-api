2289fbafb8cb831848ff93d2085f71d92e694664
Ayush <ayush-devhub@gmail.com>
2026-03-14 17:47:50 +0530

    feat(cart): add and migrate Cart and Cartitem model

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from products.models import Product
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Cart"
+        verbose_name_plural = "Carts"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "CartItem"
+        verbose_name_plural = "CartItems"
