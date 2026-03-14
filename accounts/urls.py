4860370dae1de1f9bcdd3aaf0e653f2d4828b9bc
Ayush <ayush-devhub@gmail.com>
2026-03-14 17:36:10 +0530

    feat(accounts): add JWT login and logout API views

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from django.urls import path
+
+urlpatterns = [
+    path("login/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
+    path(
+        "logout/",
+    ),
+]
