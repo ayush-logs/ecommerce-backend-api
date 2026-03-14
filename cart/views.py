88c274028e751b753dcea6c5ff28ea21fdacbdbe
Ayush <ayush-devhub@gmail.com>
2026-03-14 17:57:20 +0530

    feat(cart): add cart and cartitems CRUD API views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import CartItem, Cart
+from .serializers import CartSerializer, CartItemSerializer
 
-# Create your views here.
+
+class CartViewSet(ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
