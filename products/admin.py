ef141c5b28103acedc01e056c35b6b948623c451
Ayush <ayush-devhub@gmail.com>
2026-03-15 12:12:16 +0530

    refactor(products): modify admin panel behaviour

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 
 from products.models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "slug", "description", "created_at"]
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ["name", "description"]
+    ordering = ["name"]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "slug",
+        "price",
+        "stock",
+        "category",
+        "is_active",
+        "created_at",
+    ]
+    prepopulated_fields = {"slug": ("name",)}
+    list_filter = [
+        "is_active",
+        "category",
+    ]
+    search_fields = ["name", "description"]
+    ordering = ["name"]
