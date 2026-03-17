7823acc8ad59a1db15967e40edf0751f12954192
Ayush <ayush-devhub@gmail.com>
2026-03-18 00:25:22 +0530

    feat(cart): add cartitem quantity validation

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
+from django.core.validators import MinValueValidator
 from products.models import Product
 
 User = get_user_model()
@@ -20,7 +20,7 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
 
     class Meta:
         verbose_name = "CartItem"
