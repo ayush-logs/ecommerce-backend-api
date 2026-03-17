ade0522a0503cbeca2e31a69c0b1a8576acc538b
Ayush <ayush-devhub@gmail.com>
2026-03-18 00:29:03 +0530

    feat(cart): register route endpoints

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,20 @@
-from rest_framework.routers import SimpleRouter
-from .views import CartCreateView, CartItemViewSet
-from django.urls import path, include
+from .views import (
+    CartRetrieveView,
+    CartItemAddView,
+    CartItemUpdateView,
+    CartItemDeleteView,
+)
+from django.urls import path
 
-router = SimpleRouter()
-router.register(r"cartitems", CartItemViewSet)
+# POST   /cart/items/       → add item
+# GET    /cart/             → get current cart
+# PATCH  /cart/items/{id}/  → update quantity
+# DELETE /cart/items/{id}/  → remove item
+
+
 urlpatterns = [
-    path('', include(router.urls)),
-    path('carts/', CartCreateView.as_view(), name='create'),
+    path("cart/items/", CartItemAddView.as_view(), name="create"),
+    path("cart/", CartRetrieveView.as_view(), name="get_cart"),
+    path("cart/items/<int:pk>", CartItemUpdateView.as_view(), name="update_item"),
+    path("cart/items/<int:pk>", CartItemDeleteView.as_view(), name="delete_item"),
 ]
