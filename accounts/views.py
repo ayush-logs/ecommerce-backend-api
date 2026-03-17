d3ae8b2eaf42e0c53f652fd1e3bb9f124c035af1
Ayush <ayush-devhub@gmail.com>
2026-03-17 09:07:20 +0530

    feat(accounts): implement logout API using token blacklist

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,9 @@ from rest_framework.generics import CreateAPIView
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 User = get_user_model()
 
@@ -12,3 +15,10 @@ class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        token = RefreshToken(token=request.data["refresh"])
+        token.blacklist()
+        return Response({"detail": "Logged Out."})
