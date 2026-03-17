c56cceff095c9613a64ef2d97d6dfef2641d79d7
Ayush <ayush-devhub@gmail.com>
2026-03-17 20:27:14 +0530

    feat(accounts): add user list serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -16,3 +16,10 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
