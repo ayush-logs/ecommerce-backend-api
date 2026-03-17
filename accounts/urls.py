8f8e866d358f8cffea714ce8323cb4ead9ef2a5e
Ayush <ayush-devhub@gmail.com>
2026-03-17 09:08:24 +0530

    feat(accounts): register logout endpoint in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LogoutView
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", TokenObtainPairView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
+    path("logout/", LogoutView.as_view()),
 ]
