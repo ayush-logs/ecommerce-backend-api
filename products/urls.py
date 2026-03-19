ebb5eefc2baf23b1d8cbf2897eead2032b08c0ea
Ayush <ayush-devhub@gmail.com>
2026-03-19 20:06:04 +0530

    refactor(products): add product and category url basename

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import SimpleRouter
 from .views import ProductViewSet, CategoryViewSet
 
 router = SimpleRouter()
-router.register(r"products", ProductViewSet)
-router.register(r"categories", CategoryViewSet)
+router.register(r"products", ProductViewSet, basename="product")
+router.register(r"categories", CategoryViewSet, basename="category")
 
 urlpatterns = router.urls
