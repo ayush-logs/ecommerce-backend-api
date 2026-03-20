d28ec40f7ecd1130700c1abb203ebeb7751882df
Ayush <ayush-devhub@gmail.com>
2026-03-20 22:51:34 +0530

    feat(cart): refactor cart serializer structure

--- cart/serializers.py
+++ cart/serializers.py
@@ -4,18 +4,25 @@ from accounts.serializers import UserSerializer
 
 
 class CartSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = serializers.CharField(source="user.username", read_only=True)
 
     class Meta:
         model = Cart
         fields = ["id", "user"]
-        read_only_fields = ["user"]
 
 
-class CartItemCreateSerializer(serializers.ModelSerializer):
+class CartItemInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartItem
         fields = ["product", "quantity"]
+
+
+class CartItemOutputSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source="product.name", read_only=True)
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "product_name", "quantity"]
 
 
 class CartItemUpdateSerializer(serializers.ModelSerializer):
