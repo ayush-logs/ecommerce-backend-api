85a98a4f91380939785b71aa7f2157dac666b48e
Ayush <ayush.devhub@gmail.com>
2026-03-22 22:56:49 +0530

    feat(cart): add quantity validation

--- cart/services.py
+++ cart/services.py
@@ -1,9 +1,15 @@
 from django.db import transaction
+from rest_framework.exceptions import ValidationError
+
 from cart.models import Cart, CartItem
+from products.models import Product
 
 
 def add_item_to_cart(user, product, quantity):
     with transaction.atomic():
+        if quantity > Product.objects.get(id=product.id).stock:
+            raise ValidationError("Cart quantity is greater than stock quantity")
+
         cart, _ = Cart.objects.get_or_create(user=user)
 
         cart_item, created = CartItem.objects.get_or_create(
