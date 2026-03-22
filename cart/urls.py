9db75a8183c418227f4915e55586a03e039b9ebf
Ayush <ayush.devhub@gmail.com>
2026-03-22 23:05:41 +0530

    feat(orders): update urls

--- cart/urls.py
+++ cart/urls.py
@@ -13,7 +13,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("cart/items/", CartItemAddView.as_view(), name="create"),
+    path("cart/items/", CartItemAddView.as_view(), name="createj"),
     path("cart/", CartRetrieveView.as_view(), name="get_cart"),
     path("cart/items/<int:pk>", CartItemUpdateView.as_view(), name="update_item"),
     path("cart/items/<int:pk>", CartItemDeleteView.as_view(), name="delete_item"),
